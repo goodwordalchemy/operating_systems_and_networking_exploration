68ceb493a26024daa3c8f3efa3c85dbeca121af5
David Goldberg <goodwordachemy@gmail.com>
2019-02-03 15:39:35 -0500

    fixed bit overflow bug with casting to signed and unsigned chars

--- torrenting_client/src/messages.c
+++ torrenting_client/src/messages.c
@@ -13,16 +13,18 @@
 void encode_int_as_char(int num, char *buf, int length){
     int i;
     for (i = 0; i < length; i++){
-        buf[i] = num >> (char)((8 * (length - i - 1)) & 0xFF);
+        buf[i] = (char)(num >> ((8 * (length - i - 1)) & 0xFF));
     }
 }
 
 int decode_int_from_char(char *enc, int length){
-    int i, num;
+    int i, num, cur;
 
     num = 0;
     for (i = 0; i < length; i++){
-        num += enc[i] << (8 * (length - i - 1));
+        cur = (unsigned char) enc[i] << (8 * (length - i - 1));
+        printf("signed: %02x, unsigned: %02x\n", cur, (unsigned char) cur);
+        num += (int) cur;
     }
 
     return num;
@@ -130,8 +132,9 @@ int receive_bitfield_message(int sockfd){
     printf("received bitfield message from peer length=(%d)\n", nbytes);
     int i;
     for (i = 0; i < nbytes; i++)
-        printf("%02x", buf[i]);
+        printf("%02x", (unsigned char) buf[i]);
     printf("\n");
+
 
     return nbytes;
 }
