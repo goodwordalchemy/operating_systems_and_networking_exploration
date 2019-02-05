8ddf314c28fd7709080d9a190ef9202954f6ac60
David Goldberg <goodwordachemy@gmail.com>
2019-02-04 19:45:32 -0500

    cleaned up sending, receiving and querying bitfgield a bit

--- torrenting_client/src/messages.c
+++ torrenting_client/src/messages.c
@@ -62,31 +62,43 @@ int receive_peer_message(int sockfd, char *buf, int length){
     return nbytes;
 }
 
-int send_bitfield_message(int sockfd){
-    int i, acc;
-    char *piece_hash;
-    msg_t msg;
-    
-    int n_shift_bits = 8 - (localstate.n_pieces % 8);
+int how_many_shift_bits_in_my_bitfield(){
+    return 8 - (localstate.n_pieces % 8);
+}
+
+int how_many_bytes_in_my_bitfield(){
+    int n_shift_bits = how_many_shift_bits_in_my_bitfield();
     int n_piece_bits = localstate.n_pieces + n_shift_bits;
     int n_bitfield_bits = n_shift_bits + n_piece_bits;
     int n_bitfield_bytes = n_bitfield_bits / 8;
-    char bf_buf[n_bitfield_bytes];
 
-    char *filename = localstate.file_name;
-    char temp_filename_buffer[SHA_DIGEST_LENGTH + strlen(filename) + 1];
+    return n_bitfield_bytes;
+}
 
-    // create encoded bitfield
-    memcpy(temp_filename_buffer, filename, strlen(filename));
+int what_is_my_bitfield(){
+    int i, acc;
+    int n_shift_bits = how_many_shift_bits_in_my_bitfield();
+
     acc = 0;
     for (i = 0; i < localstate.n_pieces; i++){
-        piece_hash = localstate.piece_hashes[i];
-        memcpy(temp_filename_buffer + strlen(filename), piece_hash, SHA_DIGEST_LENGTH + 1);
-        if (does_file_exist(temp_filename_buffer))
+        if (does_file_exist(localstate.piece_hash_digests[i]))
             acc += (1 << (localstate.n_pieces - 1 - i));
     }
     acc = acc << n_shift_bits;
-    encode_int_as_char(acc, bf_buf, n_bitfield_bytes);
+
+    return acc;
+}
+
+int send_bitfield_message(int sockfd){
+    int bitfield;
+    int n_bitfield_bytes = how_many_bytes_in_my_bitfield();
+    char bf_buf[n_bitfield_bytes];
+    msg_t msg;
+    
+
+    // create encoded bitfield
+    bitfield = what_is_my_bitfield();
+    encode_int_as_char(bitfield, bf_buf, n_bitfield_bytes);
 
     // create message struct
     msg.length = n_bitfield_bytes + 1;
@@ -103,7 +115,7 @@ int receive_bitfield_message(int sockfd){
 
     // For receiving messsage
     int nbytes;
-    int n_bitfield_bytes = (localstate.n_pieces / 8 + 1);
+    int n_bitfield_bytes = how_many_bytes_in_my_bitfield();;
     int expected_msg_length = LENGTH_PREFIX_BITS + 1 + n_bitfield_bytes;
     char buf[expected_msg_length + 1];
 
