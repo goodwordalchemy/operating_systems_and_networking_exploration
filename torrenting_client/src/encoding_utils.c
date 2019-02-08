dfe760ac3c8b213a0271ef751e4330db8076cd03
David Goldberg <goodwordachemy@gmail.com>
2019-02-08 11:18:42 -0500

    added encoding module, (and changed a .h to a .o)

---
+++ torrenting_client/src/encoding_utils.c
@@ -0,0 +1,19 @@
+void encode_int_as_char(int num, char *buf, int length){
+    int i;
+    for (i = 0; i < length; i++){
+        buf[i] = (char)(num >> ((8 * (length - i - 1)) & 0xFF));
+    }
+}
+
+int decode_int_from_char(char *enc, int length){
+    int i, num, cur;
+
+    num = 0;
+    for (i = 0; i < length; i++){
+        cur = (unsigned char) enc[i] << (8 * (length - i - 1));
+        num += (int) cur;
+    }
+
+    return num;
+}
+
---
+++ torrenting_client/src/encoding_utils.h
@@ -0,0 +1,3 @@
+void encode_int_as_char(int num, char *buf, int length);
+
+int decode_int_from_char(char *enc, int length);
